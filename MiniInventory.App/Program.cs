66e38da87e3d84c87e629424045f9e40fa4caf71
Helen Toomik <helen@toomik.net>
2017-10-31 17:00:31 +0100

    feat: welcome message and instructions

--- MiniInventory.App/Program.cs
+++ MiniInventory.App/Program.cs
@@ -8,8 +8,17 @@
             var inventory = new Inventory();
             var consoleWrapper = new ConsoleWrapper();
 
+            WriteWelcomeMessage(consoleWrapper);
+
             var processor = new Processor(consoleWrapper, parser, inventory);
             processor.Process();
+        }
+
+
+        private static void WriteWelcomeMessage(ConsoleWrapper consoleWrapper)
+        {
+            consoleWrapper.WriteLine(Texts.Welcome);
+            consoleWrapper.WriteLine(Texts.Instructions);
         }
     }
 }
---
+++ MiniInventory.App/Texts.cs
@@ -0,0 +1,17 @@
+namespace MiniInventory.App
+{
+    public static class Texts
+    {
+        public const string Welcome = @"
+Välkommen till lagerhanteringen!";
+
+        public const string Instructions = @"
+Giltiga kommandon:
+För inleverans: I, följt av antal (""I5"")
+För sälj: S, följt av antal (""S2"")
+För lagersaldo: L
+För att avsluta: [Enter]
+
+Ange ett kommando:";
+    }
+}
