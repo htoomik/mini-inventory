6922281f16ccdb353dc7f57960fd83d6fabefefd
Helen Toomik <helen@toomik.net>
2017-10-31 16:59:47 +0100

    clean: spaces

--- MiniInventory.Tests/SwedishInputParserTest_Invalid.cs
+++ MiniInventory.Tests/SwedishInputParserTest_Invalid.cs
@@ -7,91 +7,91 @@ namespace MiniInventory.Tests
     {
         [Fact]
         public void UnrecognizedCommandLetter()
-		{
-			AssertThrows("X");
-		}
+        {
+            AssertThrows("X");
+        }
 
 
-		[Fact]
-		public void TooManyLetters()
-		{
-			AssertThrows("SS2");
-		}
+        [Fact]
+        public void TooManyLetters()
+        {
+            AssertThrows("SS2");
+        }
 
 
-		[Fact]
-		public void CountCommand_WithUnexpectedNumber()
-		{
-			AssertThrows("L2");
-		}
+        [Fact]
+        public void CountCommand_WithUnexpectedNumber()
+        {
+            AssertThrows("L2");
+        }
 
 
-		[Fact]
-		public void SaleCommand_WithNoNumber()
-		{
-			AssertThrows("S");
-		}
+        [Fact]
+        public void SaleCommand_WithNoNumber()
+        {
+            AssertThrows("S");
+        }
 
 
-		[Fact]
-		public void DeliveryCommand_WithNoNumber()
-		{
-			AssertThrows("I");
-		}
+        [Fact]
+        public void DeliveryCommand_WithNoNumber()
+        {
+            AssertThrows("I");
+        }
 
 
-		[Fact]
-		public void SaleCommand_WithNonIntegerNumber_Period()
-		{
-			AssertThrows("S1.2");
-		}
+        [Fact]
+        public void SaleCommand_WithNonIntegerNumber_Period()
+        {
+            AssertThrows("S1.2");
+        }
 
 
-		[Fact]
-		public void SaleCommand_WithNonIntegerNumber_Comma()
-		{
-			AssertThrows("S1,2");
-		}
+        [Fact]
+        public void SaleCommand_WithNonIntegerNumber_Comma()
+        {
+            AssertThrows("S1,2");
+        }
 
 
-		[Fact]
-		public void DeliveryCommand_WithNonIntegerNumber_Period()
-		{
-			AssertThrows("I1.2");
-		}
+        [Fact]
+        public void DeliveryCommand_WithNonIntegerNumber_Period()
+        {
+            AssertThrows("I1.2");
+        }
 
 
-		[Fact]
-		public void DeliveryCommand_WithNonIntegerNumber_Comma()
-		{
-			AssertThrows("I1,2");
-		}
+        [Fact]
+        public void DeliveryCommand_WithNonIntegerNumber_Comma()
+        {
+            AssertThrows("I1,2");
+        }
 
-		
-		[Fact]
-		public void SaleCommand_WithOtherCharactersAfterNumber()
-		{
-			AssertThrows("S1S");
-		}
-		
+        
+        [Fact]
+        public void SaleCommand_WithOtherCharactersAfterNumber()
+        {
+            AssertThrows("S1S");
+        }
+        
 
-		[Fact]
-		public void DeliveryCommand_WithOtherCharactersAfterNumber()
-		{
-			AssertThrows("I1I");
-		}		
-
-
-		[Fact]
-		public void TotallyRandomCombination()
-		{
-			AssertThrows("SIL123");
-		}
+        [Fact]
+        public void DeliveryCommand_WithOtherCharactersAfterNumber()
+        {
+            AssertThrows("I1I");
+        }        
 
 
-		private static void AssertThrows(string commandString)
-		{
-			Assert.Throws<InvalidCommandException>(() => new SwedishInputParser().Parse(commandString));
-		}
-	}
+        [Fact]
+        public void TotallyRandomCombination()
+        {
+            AssertThrows("SIL123");
+        }
+
+
+        private static void AssertThrows(string commandString)
+        {
+            Assert.Throws<InvalidCommandException>(() => new SwedishInputParser().Parse(commandString));
+        }
+    }
 }
